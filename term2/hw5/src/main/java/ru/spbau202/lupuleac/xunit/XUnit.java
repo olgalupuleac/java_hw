fba5c8a7d7273c3f66b6fb2b4b44259474ef9ce6
Olga Lupuleac <olga_lupuleac@mail.ru>
2018-05-22 17:50:32 +0300

    finished class XUnit

--- term2/hw5/src/main/java/ru/spbau202/lupuleac/xunit/XUnit.java
+++ term2/hw5/src/main/java/ru/spbau202/lupuleac/xunit/XUnit.java
@@ -1,5 +1,6 @@
 package ru.spbau202.lupuleac.xunit;
 
+import org.apache.commons.lang3.time.DurationFormatUtils;
 import ru.spbau202.lupuleac.annotations.*;
 
 import java.lang.reflect.InvocationTargetException;
@@ -16,36 +17,39 @@ public class XUnit {
     private int failed;
     private int passed;
 
-    public XUnit(){
+    public XUnit(Class<?> testClass){
+        this.testClass = testClass;
     }
 
-    public void runTests(Class<?> test){
+    public void runTests() throws IllegalAccessException, InvocationTargetException,
+            InstantiationException {
         clear();
-        separateMethods(test.getMethods());
-
+        separateMethods(testClass.getMethods());
+        launch();
+        printStatistics();
     }
 
-    private void separateMethods(Method[] methods){
-        for(Method method : methods){
-            if(method.isAnnotationPresent(Before.class)){
+    private void separateMethods(Method[] methods) {
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Before.class)) {
                 beforeTestMethods.add(method);
             }
-            if(method.isAnnotationPresent(After.class)){
+            if (method.isAnnotationPresent(After.class)) {
                 afterTestMethods.add(method);
             }
-            if(method.isAnnotationPresent(BeforeClass.class)){
+            if (method.isAnnotationPresent(BeforeClass.class)) {
                 beforeClassTestMethods.add(method);
             }
-            if(method.isAnnotationPresent(AfterClass.class)){
+            if (method.isAnnotationPresent(AfterClass.class)) {
                 afterClassTestMethods.add(method);
             }
-            if(method.isAnnotationPresent(Test.class)){
+            if (method.isAnnotationPresent(Test.class)) {
                 tests.add(method);
             }
         }
     }
 
-    private void clear(){
+    private void clear() {
         beforeClassTestMethods.clear();
         afterClassTestMethods.clear();
         beforeTestMethods.clear();
@@ -55,41 +59,69 @@ public class XUnit {
         failed = 0;
     }
 
-    private void launch() throws IllegalAccessException, InstantiationException, InvocationTargetException {
+    private void launch() throws IllegalAccessException,
+            InstantiationException, InvocationTargetException {
         Object instance = testClass.newInstance();
-        for(Method method : beforeClassTestMethods){
-                method.invoke(instance);
+        for (Method method : beforeClassTestMethods) {
+            method.invoke(instance);
         }
-        for(Method method : tests){
-            for(Method beforeMethod : beforeTestMethods){
+        for (Method method : tests) {
+            for (Method beforeMethod : beforeTestMethods) {
                 beforeMethod.invoke(instance);
             }
             Test annotation = method.getAnnotation(Test.class);
-            if(!annotation.ignore().equals("")){
+            long start = System.currentTimeMillis();
+            if (!annotation.ignore().equals("")) {
                 System.err.println("Method " + method.getName() + " is ignored.\n" + annotation.ignore());
                 continue;
             }
             Exception exception = null;
-            boolean expectedException = false;
-            boolean res = true;
+            boolean exceptionExpected = !annotation.expected().equals(Object.class);
+            boolean testResult = true;
             try {
                 method.invoke(instance);
-            }
-            catch (InvocationTargetException e){
-
-            }
-            catch (Exception e){
-                if(!e.getCause().getClass().equals(annotation.expected())){
-                    res = false;
-                }
-                else {
-                    expectedException = true;
+            } catch (InvocationTargetException e) {
+                System.err.println("Invocation of test " + method.getName()
+                        + "finished with error\n" + e.getMessage());
+                continue;
+            } catch (Exception e) {
+                exception = e;
+                if (!e.getCause().getClass().equals(annotation.expected())) {
+                    testResult = false;
                 }
             }
-            if(res && annotation.expected().equals(Object.class) && !expectedException){
-                res = false;
+            if (exceptionExpected && exception == null) {
+                testResult = false;
+            }
+            long end = System.currentTimeMillis();
+            for (Method afterMethod : afterTestMethods) {
+                afterMethod.invoke(instance);
+            }
+            if (testResult) {
+                passed++;
+                System.out.printf("Test %s passed in %s\n", method.getName(),
+                        DurationFormatUtils.formatDurationWords(end - start,
+                                true, true));
+            } else {
+                failed++;
+                System.out.printf("Test %s failed in %s\n", method.getName(),
+                        DurationFormatUtils.formatDurationWords(end - start,
+                                true, true));
+                if (exceptionExpected && exception == null) {
+                    System.out.printf(annotation.expected().getName() + " was expected\n");
+                } else {
+                    exception.printStackTrace();
+                }
             }
         }
-
+        for (Method method : afterClassTestMethods) {
+            method.invoke(instance);
+        }
     }
+
+    private void printStatistics(){
+        System.out.printf("Total number of tests: %d\nPassed: %d\nFailed: %d\n", tests.size(),
+                passed, failed);
+    }
+
 }
