89f8c1ff4128e54e80cdf17192c29c53a265357c
Olga Lupuleac <olga_lupuleac@mail.ru>
2018-06-17 20:45:46 +0300

    improved build_project.sh

--- build_project.sh
+++ build_project.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
 cd term2
-ls
-echo "end of list"
-for FILE in `ls`
-do 
-    echo "$FILE"
-    if  [ -d "${FILE}" || "$FILE" != "test1" ] ; then
-	  echo "again"
-	  echo "$FILE"
+for FILE in `ls -l`
+do
+    if test -d $FILE
+    then
       cd "$FILE"
+      echo "$FILE"
       chmod +x gradlew
-      ./gradlew build --stacktrace
-      if [ $? -ne 0 ]; then
-        exit 1
-      fi
-	  cd ..
-    else 
-	   echo "fail"
-	fi
+      ./gradlew build
+      cd ..
+   fi
 done
